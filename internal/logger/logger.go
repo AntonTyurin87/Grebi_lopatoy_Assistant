3867b35b34766c398be8924a963fce8fc417adf3
anton <AntonTyurin87@gmail.com>
2026-03-02 19:38:56 +0300

    Создана миграция для таблицы main_data

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,67 @@
 package logger
 
+import (
+	"log"
+	"os"
+	"strings"
+)
+
 // TODO файл и/или область для логирования
-// TODO логирование инициация частей сервиса
-// TODO логирование ошибок и вылетов
-// TODO логирование начала новой игровой сессии
+
+type Level int
+
+const (
+	DebugLevel Level = iota
+	InfoLevel
+	ErrorLevel
+)
+
+type Logger struct {
+	*log.Logger
+	level Level
+}
+
+func New(level string) *Logger {
+	var l Level
+	switch strings.ToLower(level) {
+	case "debug":
+		l = DebugLevel
+	case "info":
+		l = InfoLevel
+	case "error":
+		l = ErrorLevel
+	default:
+		l = InfoLevel
+	}
+
+	return &Logger{
+		Logger: log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile),
+		level:  l,
+	}
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+	if l.level <= DebugLevel {
+		l.Logger.SetPrefix("[DEBUG] ")
+		l.Println(v...)
+	}
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	if l.level <= InfoLevel {
+		l.Logger.SetPrefix("[INFO] ")
+		l.Println(v...)
+	}
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	if l.level <= ErrorLevel {
+		l.Logger.SetPrefix("[ERROR] ")
+		l.Println(v...)
+	}
+}
+
+func (l *Logger) Fatal(v ...interface{}) {
+	l.Logger.SetPrefix("[FATAL] ")
+	l.Fatal(v...)
+}
---
+++ migrations/20260302160921_create_table_main_data.sql
@@ -0,0 +1,53 @@
+-- +goose Up
+-- +goose StatementBegin
+
+
+CREATE TABLE IF NOT EXISTS main_data (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    chat_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL UNIQUE,
+    step INTEGER,
+    is_created BOOL,
+    profession TEXT,
+    gender INTEGER,
+    world TEXT,
+    marriage BOOL,
+    childs INTEGER,
+    wishes INTEGER,
+    turn INTEGER,
+    salary INTEGER,
+    salary_extra_name TEXT,
+    salary_extra INTEGER,
+    cost_house INTEGER,
+    cost_food INTEGER,
+    cost_transport INTEGER,
+    cost_cloth INTEGER,
+    cost_extra_name TEXT,
+    cost_extra INTEGER,
+    total_income INTEGER,
+    total_outcome INTEGER,
+    flow INTEGER,
+    cash INTEGER,
+    menu_id INTEGER,
+    id_last_active INTEGER,
+    debt INTEGER,
+    created_at TEXT,
+    updated_at TEXT
+);
+
+-- Индексы
+CREATE INDEX IF NOT EXISTS idx_main_data_user_id ON main_data(user_id);
+CREATE INDEX IF NOT EXISTS idx_main_data_created_at ON main_data(created_at);
+CREATE INDEX IF NOT EXISTS idx_main_data_updated_at ON main_data(updated_at);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS main_data;
+
+-- Удаление индексов
+DROP INDEX IF EXISTS idx_main_data_updated_at;
+DROP INDEX IF EXISTS idx_main_data_created_at;
+DROP INDEX IF EXISTS idx_main_data_user_id;
+-- +goose StatementEnd
